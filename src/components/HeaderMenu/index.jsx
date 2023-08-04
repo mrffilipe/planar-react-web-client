2bc8be60a573027957de9727abe95f17761fa580
Filipe Moreira Ferracioli <mrffilipe@outlook.com>
2023-08-03 23:50:27 -0300

    Add modal notifications

--- src/components/HeaderMenu/index.jsx
+++ src/components/HeaderMenu/index.jsx
@@ -1,12 +1,56 @@
 import styles from "./styles.module.css";
 
 import NotificationIcon from "../../assets/icons/notification.svg";
+import { useState } from "react";
 
 const HeaderMenu = () => {
+    const [showNotification, setShowNotification] = useState(false);
+
     return (
         <header>
             <span className={styles.tab_title}>Dashboard</span>
             <div className={styles.account_details}>
+                {
+                    showNotification ? (
+                        <div className={styles.modal_notification}>
+                            <span>Notificações</span>
+                            <ul className={styles.list_notifications}>
+                                <li>
+                                    <h2>Energia solar</h2>
+                                    <p>Texto da notificação que se refere a instalação de energia solar</p>
+                                    <span>Enviado há 2h</span>
+                                </li>
+                                <li>
+                                    <h2>Mensagem recebida</h2>
+                                    <p>Texto da notificação que se refere a mensagem recebida</p>
+                                    <span>Enviado há 1m</span>
+                                </li>
+                                <li>
+                                    <h2>Mensagem recebida</h2>
+                                    <p>Texto da notificação que se refere a mensagem recebida</p>
+                                    <span>Enviado há 7d</span>
+                                </li>
+                                <li>
+                                    <h2>Energia solar</h2>
+                                    <p>Texto da notificação que se refere a instalação de energia solar</p>
+                                    <span>Enviado há 2h</span>
+                                </li>
+                                <li>
+                                    <h2>Mensagem recebida</h2>
+                                    <p>Texto da notificação que se refere a mensagem recebida</p>
+                                    <span>Enviado há 1m</span>
+                                </li>
+                                <li>
+                                    <h2>Mensagem recebida</h2>
+                                    <p>Texto da notificação que se refere a mensagem recebida</p>
+                                    <span>Enviado há 7d</span>
+                                </li>
+                            </ul>
+                        </div>
+                    ) : (
+                        <></>
+                    )
+                }
                 <img className={styles.notification} src={NotificationIcon} alt="Notification" />
                 <div className={styles.user_details}>
                     <div className={styles.profile_picture}></div>
