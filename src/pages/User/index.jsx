47d35a2dfa093019fffc087900e5cf0b6169f56d
Filipe Moreira Ferracioli <mrffilipe@outlook.com>
2023-09-05 04:55:32 -0300

    Adjusts User page

--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -1,6 +1,9 @@
 import styles from "./styles.module.css";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
 import { Edit } from "@mui/icons-material";
 
 import TextButton from "../../components/TextButton";
@@ -8,6 +11,23 @@ import { SubmitInput, TextInput } from "../../components/Inputs";
 
 const User = () => {
     const [change, setChange] = useState(true);
+    const [user, setUser] = useState({});
+
+    const { userId } = useParams();
+
+    useEffect(() => {
+        const startFetching = async () => {
+            const response = await axios.get(`https://localhost:7104/api/useraccount/find-user-by-id?id=${userId}`);
+
+            if (response.status !== 200) {
+                throw new Error();
+            }
+
+            setUser(response.data);
+        };
+
+        startFetching();
+    }, []);
 
     return (
         <div className={styles.user_container}>
@@ -19,9 +39,9 @@ const User = () => {
                 <form className={styles.form}>
                     <fieldset>
                         <legend>Informações do usuário</legend>
-                        <TextInput label="Nome" required />
-                        <TextInput label="Sobrenome" required />
-                        <TextInput label="Email" required />
+                        <TextInput label="Nome" value={user.firstName} required />
+                        <TextInput label="Sobrenome" value={user.lastName} required />
+                        <TextInput label="Email" value={user.email} required />
                     </fieldset>
                     {
                         change &&
